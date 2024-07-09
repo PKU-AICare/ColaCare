4734c11b206eb48ded4fce0a4b6e8b1ce2496b8d
fieldry <zixiangwang.buaa.edu.cn>
2024-07-09 13:35:14 +0800

    feat: update prompt template

--- template.py
+++ template.py
@@ -29,7 +29,26 @@ Here is an example of the format you should output:
 Please respond with only the information you want to search in JSON format without any additional information:
 ''')
 
-ensemble_evaluate_system = '''You are a helpful medical expert with extensive medical knowledge. I will provide you with electronic health data of an End-Stage Renal Disease (ESRD) patient and some analysis results from several AI models. Every model's analysis results include mortality risk and feature importance weight. Your task is to ensemble analysis results of all models and provide your prediction result of mortality risk using the relevant documents.'''
+retcare_system = '''You are a helpful medical expert with extensive medical knowledge. I will provide you with electronic health data of an End-Stage Renal Disease (ESRD) patient and some analysis results from our model, with the analysis results including mortality risk and feature importance weight. Your task is to analyze if the model's analysis results are reasonable using the relevant documents. Your analysis should be based on the relevant documents, and do not include any unsupported conclusions.'''
+
+retcare_user = Template('''
+Here are the relevant documents:
+{{context}}
+
+Here is the healthcare context, including the patient's basic information, analysis results of AI models and similar patients' information:
+{{hcontext}}
+
+Note that the analysis results from the AI model are not all correct. Please analyze by following the steps below based on relevant documents:
+1. Which of the relevant documents support the AI model's analysis results? Which do not? Please directly cite sentences or paragraphs from the documents' content in your explanation.
+2. Do you think whether the AI model's analysis results are reasonable? The prediction of mortality risk is higher or lower than the actual risk? Please provide your analysis based on the relevant documents, and disjudge the analysis results of the AI model if necessary.
+3. Please provide your prediction of mortality risk as a number between 0 and 1.
+
+Please think step-by-step and analyze the results based on relevant documents. Do not include any unsupported conclusions. Generate your output formatted as Dict{"result": Result, "explanation": Str(Your analysis)} without any additional information, where result is a number between 0 and 1 indicating the mortality risk prediction:
+''')
+
+ensemble_evaluate_system_esrd = '''You are a helpful medical expert with extensive medical knowledge. I will provide you with electronic health data of an End-Stage Renal Disease (ESRD) patient and some analysis results from several AI models. Every model's analysis results include mortality risk and feature importance weight. Your task is to ensemble analysis results of all models and provide your prediction result of mortality risk using the relevant documents.'''
+
+ensemble_evaluate_system_icu = '''You are a helpful medical expert with extensive medical knowledge. I will provide you with electronic health data of an End-Stage Renal Disease (ESRD) patient and some analysis results from several AI models. Every model's analysis results include mortality risk and feature importance weight. Your task is to ensemble analysis results of all models and provide your prediction result of mortality risk using the relevant documents.'''
 
 ensemble_evaluate_user = Template('''
 Here are the relevant documents:
@@ -38,15 +57,18 @@ Here are the relevant documents:
 Here is the healthcare context, including the patient's basic information, analysis results of AI models and similar patients' information:
 {{hcontext}}
 
-Note that the analysis results from the AI models are not all correct. Please analyze by following the steps below based on relevant documents:
-1. Please list the documents supporting the analysis results for each AI model separately and directly cite sentences or paragraphs from the documents' content in your explanation.
-2. Please analyze whether the analysis results of each AI model are relatively reasonable, high, or low.
-3. Please analyze the patient's information, based on relevant documents and the analysis results of all AI models, and provide your prediction result of mortality risk as a number between 0 and 1. Do not give the mean value of all models' analysis results.
-
-Please think step-by-step and analyze the results based on relevant documents. Do not include any unsupported conclusions. Generate your output formatted as Dict{"result": Result, "explanation": Str(Your analysis)} without any additional information, where result is a number between 0 and 1 indicating the mortality risk prediction:
+Note that the analysis results from the AI model are not all correct. Please think step-by-step and analyze the results based on relevant documents. Generate your output formatted as Dict{"result": Result, "explanation": Str(Your analysis)} without any additional information, where result is a number between 0 and 1 indicating the mortality risk prediction, and explanation is your analysis following the template below:
+## Summary
+    Please describe your task, summarize the patient's basic information and health status, and restate the AI model's prediction results along with the feature importance and partial statistic information.
+## Documents Analysis
+    Please analyze the important features identified by the models, determine whether the identified features are reasonable. If they are reasonable, provide and cite relevant literature, include quotations from the sources, and explain the reasoning. If the features are not reasonable, provide and rank important features in your analysis, cite relevant documents and explain. Ensure that the features you identify are present in the dataset.
+## Prediction Analysis
+    Please evaluate the AI models' prediction results: too low, too high, or reasonable? If it's not reasonable, please provide your own prediction results, represented as a float number between 0 and 1.
 ''')
 
-ensemble_select_system = '''You are a helpful medical expert with extensive medical knowledge. I will provide you with electronic health data of a End-Stage Renal Disease (ESRD) patient and some analysis results from several AI models. Every model's analysis results include mortality risk and feature importance weight. Your task is to ensemble analysis results of all models and select one result of models as your prediction result based on the relevant documents.'''
+ensemble_select_system_esrd = '''You are a helpful medical expert with extensive medical knowledge. I will provide you with electronic health data of a End-Stage Renal Disease (ESRD) patient and some analysis results from several AI models. Every model's analysis results include mortality risk and feature importance weight. Your task is to ensemble analysis results of all models and select one result of models as your prediction result based on the relevant documents.'''
+
+ensemble_select_system_icu = '''You are a helpful medical expert with extensive medical knowledge. I will provide you with electronic health data of a End-Stage Renal Disease (ESRD) patient and some analysis results from several AI models. Every model's analysis results include mortality risk and feature importance weight. Your task is to ensemble analysis results of all models and select one result of models as your prediction result based on the relevant documents.'''
 
 ensemble_select_user = Template('''
 Here are the relevant documents:
@@ -55,10 +77,11 @@ Here are the relevant documents:
 Here is the healthcare context, including the patient's basic information, analysis results of AI models and similar patients' information:
 {{hcontext}}
 
-Note that the analysis results from the AI models are not all correct. Please analyze by following the steps below based on relevant documents:
-1. Please list the documents supporting the analysis results for each AI model separately and directly cite sentences or paragraphs from the documents' content in your explanation.
-2. Please analyze whether the analysis results of each AI model are relatively reasonable, high, or low.
-3. Please analyze the patient's information, relevant documents, and the analysis results of all AI models, and choose one of the results as the final output. If you think the results of all models are unreasonable, please provide your prediction result of mortality risk as a number between 0 and 1.
-
-Please think step-by-step and analyze the results based on relevant documents. Do not include any unsupported conclusions. Generate your output formatted as Dict{"result": Result, "explanation": Str(Your analysis)} without any additional information, where result is a number between 0 and 1 indicating the mortality risk prediction:
+Note that the analysis results from the AI model are not all correct. Please think step-by-step and analyze the results based on relevant documents. Generate your output formatted as Dict{"result": Result, "explanation": Str(Your analysis)} without any additional information, where result is a number between 0 and 1 indicating the mortality risk prediction, and explanation is your analysis following the template below:
+## Summary
+    Please describe your task, summarize the patient's basic information and health status, and restate the AI model's prediction results along with the feature importance and partial statistic information.
+## Documents Analysis
+    Please analyze the important features identified by the models, determine whether the identified features are reasonable. If they are reasonable, provide and cite relevant literature, include quotations from the sources, and explain the reasoning. If the features are not reasonable, provide and rank important features in your analysis, cite relevant documents and explain. Ensure that the features you identify are present in the dataset.
+## Prediction Analysis
+    Please evaluate the AI models' prediction results: too low, too high, or reasonable? Choose one of them as your result, represented as a float number between 0 and 1.
 ''')
