437e33480795e57d1452a486cb37c4bdaf6b9e1b
Zixiang Wang <19241078@buaa.edu.cn>
2024-10-11 16:16:50 +0800

    fix: get the last visit

--- pyehr/ehrdatasets/loader/unpad.py
+++ pyehr/ehrdatasets/loader/unpad.py
@@ -9,10 +9,10 @@ def unpad_y(y_pred, y_true, lens):
     y_pred, y_true, lens = y_pred.to(device), y_true.to(device), lens.to(device)
     y_pred_unpad = unpad_sequence(y_pred, batch_first=True, lengths=lens)
     # y_pred_stack = torch.vstack(y_pred_unpad).squeeze(dim=-1)
-    y_pred_stack = torch.vstack([y[0] for y in y_pred_unpad]).squeeze(dim=-1)
+    y_pred_stack = torch.vstack([y[-1] for y in y_pred_unpad]).squeeze(dim=-1)
     y_true_unpad = unpad_sequence(y_true, batch_first=True, lengths=lens)
     # y_true_stack = torch.vstack(y_true_unpad).squeeze(dim=-1)
-    y_true_stack = torch.vstack([y[-1][0] for y in y_true_unpad]).squeeze(dim=-1)
+    y_true_stack = torch.vstack([y[-1] for y in y_true_unpad]).squeeze(dim=-1)
     return y_pred_stack.to(raw_device), y_true_stack.to(raw_device)
 
 def unpad_batch(x, y, lens):
