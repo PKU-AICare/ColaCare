3a001d02425a20fb0c84002677d890230d01c29a
Zixiang Wang <19241078@buaa.edu.cn>
2024-10-11 10:58:29 +0800

    fix: use the last visit for prediction

--- pyehr/ehrdatasets/loader/unpad.py
+++ pyehr/ehrdatasets/loader/unpad.py
@@ -12,7 +12,7 @@ def unpad_y(y_pred, y_true, lens):
     y_pred_stack = torch.vstack([y[0] for y in y_pred_unpad]).squeeze(dim=-1)
     y_true_unpad = unpad_sequence(y_true, batch_first=True, lengths=lens)
     # y_true_stack = torch.vstack(y_true_unpad).squeeze(dim=-1)
-    y_true_stack = torch.vstack([y[0][0] for y in y_true_unpad]).squeeze(dim=-1)
+    y_true_stack = torch.vstack([y[-1][0] for y in y_true_unpad]).squeeze(dim=-1)
     return y_pred_stack.to(raw_device), y_true_stack.to(raw_device)
 
 def unpad_batch(x, y, lens):
@@ -21,8 +21,8 @@ def unpad_batch(x, y, lens):
     lens = lens.detach().cpu()
     x_unpad = unpad_sequence(x, batch_first=True, lengths=lens)
     # x_stack = torch.vstack(x_unpad).squeeze(dim=-1)
-    x_stack = torch.vstack([x[0] for x in x_unpad]).squeeze(dim=-1)
+    x_stack = torch.vstack([x[-1] for x in x_unpad]).squeeze(dim=-1)
     y_unpad = unpad_sequence(y, batch_first=True, lengths=lens)
     # y_stack = torch.vstack(y_unpad).squeeze(dim=-1)
-    y_stack = torch.vstack([y[0][0] for y in y_unpad]).squeeze(dim=-1)
+    y_stack = torch.vstack([y[-1][0] for y in y_unpad]).squeeze(dim=-1)
     return x_stack.numpy(), y_stack.numpy()
