a4d9a71eb550cd01bfec48f15a018a8682bee7bb
Zixiang Wang <19241078@buaa.edu.cn>
2024-10-13 00:27:16 +0800

    fix: update prompt

--- utils/template_readmission.py
+++ utils/template_readmission.py
@@ -2,7 +2,7 @@ from liquid import Template
 
 
 ## Phase 1
-doctor_review_system = """You are an experienced doctor with extensive medical knowledge. I will provide you with multivariate time-series electronic health record for a patient, which is a structured collection of patient information comprising multiple clinical variables measured at various time points across multiple patient visits, represented as sequences of numerical values for each feature. I will also provide some AI model analyses for this patient, including the 30-day readmission prediction results and feature importance weights. The 30-day readmission prediction result refers to the likelihood of readmission after 30 days since discharge. The greater the feature importance weight, the greater the impact of that feature on the patient's readmission likelihood. When the patient's likelihood of readmission is low, this impact is positive; conversely, when the likelihood is high, the impact is negative. I will also retrieve relevant medical knowledge based on the AI model's analysis results and provide it to you. Please summarize the patient's condition based on the information and generate an analytical review."""
+doctor_review_system = """You are an experienced doctor with extensive medical knowledge. I will provide you with multivariate time-series electronic health record for a patient, which is a structured collection of patient information comprising multiple clinical variables measured at various time points across multiple patient visits, represented as sequences of numerical values for each feature. I will also provide some AI model analyses for this patient, including the 30-day readmission prediction results and feature importance weights. The 30-day readmission prediction result refers to the likelihood of readmission after 30 days since discharge. The greater the feature importance weight, the greater the impact of that feature on the patient's readmission likelihood. When the patient's likelihood of readmission is low, this impact is positive; conversely, when the likelihood is high, the impact is negative. I will also retrieve relevant medical knowledge based on the AI model's analysis results and provide it to you. Please summarize the patient's condition based on multivariate time-series electronic health record, mortality risk and especially feature importance weights, and generate an analytical review."""
 
 doctor_review_user = Template("""Here is the relevant medical knowledge:
 {{context}}
@@ -36,11 +36,11 @@ You need to read all doctors' opinions carefully and analyze whether their opini
 Next, please write a synthesized report including the following:
 1. A direct statement: in your opinion, whether the patient's risk of 30-day readmission is high or not.
 2. A summary of the patient's condition and the characteristics of the patient that you think are worthy of attention. Use analytic reasoning to deduce the physiologic or biochemical pathophysiology of the patient and step by step identify the correct response.
-3. List of supporting evidence: [Doctor 0's opinion or relevant literature retrieved, Doctor 1's opinion or relevant literature retrieved, ...].
+3. List of supporting evidence. Please output detailed content from some of the evidence provided by the doctors or some analysis results of the doctors. Do not just list the doctor's name.
 
 Here are two examples of the format you should output:
-{"answer":"In my opinion, the patient has a high likelihood of 30-day readmission.", "report": "...", "evidences": ["Doctor 0's ... ", "Doctor 1's ..."]}.
-{"answer":"In my opinion, the patient has a low likelihood of 30-day readmission.", "report": "...", "evidences": ["Doctor 0's ... ", "Doctor 1's ..."]}.
+{"answer":"In my opinion, the patient has a high likelihood of 30-day readmission.", "report": "...", "evidences": ["...", "..."]}.
+{"answer":"In my opinion, the patient has a low likelihood of 30-day readmission.", "report": "...", "evidences": ["...", "..."]}.
 
 Respond in JSON format without any additional content:
 """)
@@ -102,32 +102,39 @@ Respond in JSON format without any additional content:
 """)
 
 
-collaborative_summary_system = """You are an authoritative expert in the medical field. You are organizing a collaborative consultation. Now several doctors have made analysis and judgments on a patient's condition. Your task is to analyze the rationality of each doctor's opinion and summarize the opinions you think are reasonable to obtain a synthesized report for the patient."""
+collaborative_summary_system = """You are an authoritative expert in the medical field. You are organizing a collaborative consultation. Now several doctors have made analysis and judgments on a patient's condition. Your task is to judge whether everyone has reached a consensus on the diagnosis of the patient based on the analysis statements of each doctor and then analyze the rationality of each doctor's opinion and summarize the opinions you think are reasonable to obtain a synthesized report for the patient."""
 
 collaborative_summary_user = Template("""In the previous discussion, you took into account the opinions of all the doctors and obtained a synthesized report about the patient, which is listed as follows:
 {{latest_info}}
 
-Then, all the doctors offer new perspectives and opinions on your synthesized report.
-In each doctor's statement, they first expressed whether they agreed with your statement in the previous synthesized report and gave the degree of confidence in their own judgment. Then, they further elaborated on their views by stating reasons and listing relevant literature.
+In response to the patient's synthesized report, several doctors put forward their own opinions and reasons.
+In each doctor's statement, they first express whether they agree with your statement in the previous synthesized report and give the confidence level on their own judgment. Then, they further elaborate on their views by stating reasons and listing relevant literatures.
 The following are their opinions:
 {{doctor_info}}
 
-You need to consider all the doctors' new ideas and modify your original synthesized report.
-1. If a doctor expresses strong opposition to your previous statement, you need to focus on his reasons and arguments and think carefully about whether you need to reconsider the 30-day readmission risk of the patient and modify your synthesized report accordingly.
+Now, you need to judge whether the next round of discussion is needed based on each doctor's statement. Considering the following four cases:
+1. If all doctors agree with the previous synthesized report, there is no need to continue the discussion.
+2. If some doctors disagree with the previous report, but they are not confident in their judgment and have not listed convincing evidence, there is no need to continue the discussion.
+3. If some doctors strongly oppose the previous report and you think their evidence is worth discussing, please continue the discussion.
+4. If most doctors disagree with the previous report, please continue the discussion.
+
+If you think the discussion should continue, you need to analyze the rationality of each doctor's opinions and summarize the opinions you think are reasonable to obtain a synthesized report for the patient. You shold follow the following cases:
+1. If a doctor expresses strong opposition to your previous statement, you need to focus on his reasons and arguments and think carefully about whether you need to reconsider mortality risk of the patient and modify your synthesized report accordingly.
 2. If a doctor expresses opposition but also has some doubts about his own opinion, you need to consider his opinion, but you can stick to your original opinion.
 3. If a doctor expresses agreement, then you do not need to modify your original synthesized report based on his opinion.
 
 Please output the following three contents in JSON format:
-1. Your statement of 30-day readmission risk of the patient.
-2. Your revised synthesized report. Please create a concise and clear summary of the patient's health status. Your summary should be informative and beneficial for healthcare prediction tasks, such as the 30-day readmission prediction. Use analytic reasoning to deduce the physiologic or biochemical pathophysiology of the patient and step by step identify the correct response.
-3. Your reasons for revision. The format of the reason for revision is: which doctor's opinion or relevant literature you refer to, and which original opinions you modify. Please output detailed content, don't just list the doctor's name.
+1. Whether to continue the discussion or not. Please respond with `Yes` or `No`.
+2. Your statement of mortality risk of the patient, i.e., whether the mortality risk of the patient is higher or not.
+3. Your revised synthesized report. Please create a concise and clear summary of the patient's health status. Your summary should be informative and beneficial for healthcare prediction tasks, such as in-hospital mortality prediction. Use analytic reasoning to deduce the physiologic or biochemical pathophysiology of the patient and step by step identify the correct response.
+4. Your reasons for revision. The format of the reason for revision is: which doctor's opinion or relevant literature you refer to, and which original opinions you modify. Please output detailed content, don't just list the doctor's name.
 
 Here is an example of the revised synthesized report, please follow this format:
 35-year-old female was diagnosed with chronic glomerulonephritis five years ago. Chronic kidney disease progressed to end stage renal disease (ESRD) over last year. Client was started on automated peritoneal dialysis six months ago. She developed fever, vomiting, and abdominal pain 1 day ago. Current assessment: T 39.1 °C, (102.40 F), HR 104, RR 16, B/P 145/ 87 mmHg. Weight: 66.8 Kg/147 lbs. NKDA. Periumbilical tenderness, defense and rebound. Erythema and creamy, yellow exudate around peritoneal dialysis catheter exit site. Dialysate effluent is cloudy yellow. Peritoneal effluent culture was obtained. Labs sent. The patient's complex medical history and the presence of multiple comorbidities suggest a high risk for 30-day readmission. Close monitoring and management of these conditions are critical for reducing the risk of readmission.
 
 Here are two examples of the format you should output:
-{"answer": "In my opinion, the patient has a high risk of 30-day readmission.", "report": "...", "reasons": ["reason1 ...", "reason2 ..."]}.
-{"answer": "In my opinion, the patient has a low risk of 30-day readmission.", "report": "...", "reasons": ["reason1 ...", "reason2 ..."]}.
+{"action": "No", "answer": "In my opinion, the patient has a high risk of mortality.", "report": "...", "reasons": ["reason1 ...", "reason2 ..."]}
+{"action": "Yes", "answer": "In my opinion, the patient has a low risk of mortality.", "report": "...", "reasons": ["reason1 ...", "reason2 ..."]}
 
 Respond in JSON format without any additional content:
 """)
