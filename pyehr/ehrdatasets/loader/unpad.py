2c137e98feb2416cff5da9c1bb2008c59ffe8b8b
Fieldry <wangzx@stu.pku.edu.cn>
2025-03-13 16:33:42 +0800

    fix: unpad y for n-1 prediction

--- pyehr/ehrdatasets/loader/unpad.py
+++ pyehr/ehrdatasets/loader/unpad.py
@@ -14,8 +14,10 @@ def unpad_y(y_pred, y_true, lens, task="outcome"):
     # y_true_stack = torch.vstack(y_true_unpad).squeeze(dim=-1)
     if task == "readmission":
         y_true_stack = torch.vstack([y[-1][2] for y in y_true_unpad]).squeeze(dim=-1)
-    else:
+    elif task == "outcome":
         y_true_stack = torch.vstack([y[-1][0] for y in y_true_unpad]).squeeze(dim=-1)
+    else:
+        y_true_stack = torch.vstack([y[-1] for y in y_true_unpad]).squeeze(dim=-1)
     return y_pred_stack.to(raw_device), y_true_stack.to(raw_device)
 
 def unpad_batch(x, y, lens):
